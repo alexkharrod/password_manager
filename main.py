6b1f689e193eb42cb20940b34df0645405305efd
alexharrod <alexkharrod@gmail.com>
2024-09-22 11:17:17 -0400

    Added messageboxes

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
+
 from tkinter import *
 from tkinter.ttk import *
+from tkinter import messagebox
 
 
-
-
-style = Style()
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_btn():
+    web = website_entry.get()
+    eml = email_user_entry.get()
+    pswd = password_entry.get()
+
+    if web == "" or pswd == "":
+        messagebox.showinfo(message="Please don't leave any fields empty!")
+    else:
+        save(web, eml, pswd)
+        print(f"{web} , {eml} , {pswd}")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+def save(website, email,  password):
+    with open('file.txt', 'a') as f:
+        f.write(f"{website} | {email} | {password}\n")
+    messagebox.showinfo(message="Password Saved Successfully")
+
+
+
+
+
+
+    # create function called save
+    # add button command that appends the data to the end of a text file as a newline, clears the text
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Style()
@@ -29,6 +54,7 @@ email_user_label = Label(text="Email / Username:")
 email_user_label.grid(column=0, row=2)
 email_user_entry = Entry(width=39)
 email_user_entry.grid(column=1, row=2, columnspan=2)
+email_user_entry.insert(0, 'alexkharrod@gmail.com')
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
@@ -38,7 +64,7 @@ password_entry.grid(column=1, row=3)
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=add_btn)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
